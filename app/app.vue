36629b7755df4c865a3fbf2099b441e44f428985
Diogo Lima <a15317@alunos.ipca.pt>
2025-03-17 23:28:18 +0000

    migrate to nuxt-ui3

--- app/app.vue
+++ app/app.vue
@@ -43,96 +43,96 @@ const items = [
 </script>
 
 <template>
-  <UContainer class="min-h-screen flex flex-col my-4">
-    <div class="mb-2 text-right">
-      <UButton
-        square
-        variant="ghost"
-        color="neutral"
-        :icon="
-          $colorMode.preference === 'dark' || $colorMode.preference === 'system'
-            ? 'i-heroicons-moon'
-            : 'i-heroicons-sun'
-        "
-        @click="isDarkMode = !isDarkMode"
-      />
-    </div>
-
-    <UCard>
-      <template #header>
-        <h3 class="text-lg font-semibold leading-6">
-          <NuxtLink to="/">
-            Atidone
-          </NuxtLink>
-        </h3>
+  <UApp>
+    <UContainer class="min-h-screen flex flex-col my-4">
+      <div class="mb-2 text-right">
         <UButton
-          v-if="!loggedIn"
-          to="/api/auth/github"
-          icon="i-simple-icons-github"
-          label="Login with GitHub"
           color="neutral"
-          size="xs"
-          external
+          :icon="
+            $colorMode.preference === 'dark' || $colorMode.preference === 'system'
+              ? 'i-heroicons-moon'
+              : 'i-heroicons-sun'
+          "
+          @click="isDarkMode = !isDarkMode"
         />
-        <div
-          v-else
-          class="flex flex-wrap -mx-2 sm:mx-0"
-        >
-          <UButton
-            to="/todos"
-            icon="i-heroicons-list-bullet"
-            label="Todos"
-            :color="$route.path === '/todos' ? 'primary' : 'gray'"
-            variant="ghost"
-          />
-          <UButton
-            to="/optimistic-todos"
-            icon="i-heroicons-sparkles"
-            label="Optimistic Todos"
-            :color="$route.path === '/optimistic-todos' ? 'primary' : 'neutral'"
-            variant="ghost"
-          />
-          <UDropdown
-            v-if="user"
-            :items="items"
-          >
-            <UButton
-              color="neutral"
-              variant="ghost"
-              trailing-icon="i-heroicons-chevron-down-20-solid"
-            >
-              <UAvatar
-                :src="`https://github.com/${user.login}.png`"
-                :alt="user.login"
-                size="3xs"
-              />
-              {{ user.login }}
-            </UButton>
-          </UDropdown>
-        </div>
-      </template>
-      <NuxtPage />
-    </UCard>
+      </div>
 
-    <footer class="text-center mt-2">
-      <NuxtLink
-        href="https://github.com/atinux/atidone"
-        target="_blank"
-        class="text-sm text-gray-500 hover:text-gray-700"
-      >
-        GitHub
-      </NuxtLink>
-      ·
-      <NuxtLink
-        href="https://twitter.com/atinux"
-        target="_blank"
-        class="text-sm text-gray-500 hover:text-gray-700"
-      >
-        Twitter
-      </NuxtLink>
-    </footer>
-  </UContainer>
-  <UNotifications />
+      <UCard>
+        <template #header>
+          <div class="flex justify-between">
+            <h3 class="text-lg font-semibold leading-6">
+              <NuxtLink to="/">
+                Atidone
+              </NuxtLink>
+            </h3>
+            <UButton
+              v-if="!loggedIn"
+              to="/api/auth/github"
+              icon="i-simple-icons-github"
+              label="Login with GitHub"
+              size="xs"
+              external
+            />
+            <div
+              v-else
+              class="flex flex-wrap -mx-2 sm:mx-0"
+            >
+              <UButton
+                to="/todos"
+                icon="i-heroicons-list-bullet"
+                label="Todos"
+                :color="$route.path === '/todos' ? 'primary' : 'neutral'"
+                variant="ghost"
+              />
+              <UButton
+                to="/optimistic-todos"
+                icon="i-heroicons-sparkles"
+                label="Optimistic Todos"
+                :color="$route.path === '/optimistic-todos' ? 'primary' : 'neutral'"
+                variant="ghost"
+              />
+              <UDropdownMenu
+                v-if="user"
+                :items="items"
+              >
+                <UButton
+                  color="neutral"
+                  variant="ghost"
+                  trailing-icon="i-heroicons-chevron-down-20-solid"
+                >
+                  <UAvatar
+                    :src="`https://github.com/${user.login}.png`"
+                    :alt="user.login"
+                    size="3xs"
+                  />
+                  {{ user.login }}
+                </UButton>
+              </UDropdownMenu>
+            </div>
+          </div>
+        </template>
+        <NuxtPage />
+      </UCard>
+
+      <footer class="text-center mt-2">
+        <NuxtLink
+          href="https://github.com/atinux/atidone"
+          target="_blank"
+          class="text-sm text-gray-500 hover:text-gray-700"
+        >
+          GitHub
+        </NuxtLink>
+        ·
+        <NuxtLink
+          href="https://twitter.com/atinux"
+          target="_blank"
+          class="text-sm text-gray-500 hover:text-gray-700"
+        >
+          Twitter
+        </NuxtLink>
+      </footer>
+    </UContainer>
+  </UApp>
 </template>
 
 <style lang="postcss">
